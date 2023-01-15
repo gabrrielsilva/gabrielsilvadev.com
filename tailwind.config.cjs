8501dbe49bbb69fa097b874c494708541652560c
gabrrielsilva <gabrielsilvaev@gmail.com>
2023-01-15 16:03:16 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				matterRegular: ['Matter Regular'],
+				matterSemiBold: ['Matter SemiBold'],
+				matterHeavy: ['Matter Heavy']
+			}
+		},
+	},
+	plugins: [],
+};
