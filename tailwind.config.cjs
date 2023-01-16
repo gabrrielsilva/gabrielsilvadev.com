c11a8317238fad6598f33bc37a76d8ffd6a8c8c6
gabrrielsilva <gabrielsilvaev@gmail.com>
2023-01-15 23:24:52 -0300

    feat: home page and lifecycle diagram

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: {
+				brand: '#00ff5f'
+			},
 			fontFamily: {
 				matterRegular: ['Matter Regular'],
 				matterSemiBold: ['Matter SemiBold'],
