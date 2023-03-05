e8359e83c813c07662ab7588091a6fd2e24b057a
gabrrielsilva <gabrielsilvaev@gmail.com>
2023-03-05 16:26:25 -0300

    feat: update design services section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,8 @@ module.exports = {
 		extend: {
 			colors: {
 				brand: '#06f784',
-				secondaryBrand: '#fcfbf8'
+				secondaryBrand: '#fcfbf8',
+				tertiaryBrand: '#fff059'
 			},
 			fontFamily: {
 				matterRegular: ['Matter Regular'],
@@ -14,7 +15,25 @@ module.exports = {
 			},
 			animation: {
         'spin-slow': 'spin 3s linear infinite',
+				'scroll': 'scroll 20s linear infinite',
+				'bounce-slow': 'bounce 3s infinite'
       },
+			keyframes: {
+				scroll: {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(calc(-250px * 7))' }
+				},
+				bounce: {
+					'0%, 100%': {
+						transform: 'translateY(-5%)',
+						'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)'
+					},
+					'50%': {
+						transform: 'translateY(0)',
+						'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)'
+					}
+				}
+			}
 		},
 	},
 	plugins: [require('tailwind-scrollbar')],
